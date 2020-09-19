61769b51d563951d14067cb4e0eb9398262de115
Darthveloper21 <hienvungoc01@gmail.com>
2020-09-19 22:43:45 +0700

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import words_handler.Dictionary;
 import words_handler.Word;
 
 public class Main {
     /**
-     *
-     * @param args
+     * dictionary2vn.
+     * @param args do nothing.
      */
     public static void main(String[] args) {
         Word word = new Word("hien", "deptrai");
-        System.out.println(word.getWord_target() + " " + word.getWord_explain());
+        Dictionary dict = new Dictionary();
+        dict.addWord(word);
+        System.out.println(dict.getWord(0).getWord_target() + ": " + dict.getWord(0).getWord_explain());
     }
 }
