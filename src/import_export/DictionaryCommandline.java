03cc3373fedd0a4fde5993e3628a1f737ebe0b79
Darthveloper21 <hienvungoc01@gmail.com>
2020-09-20 10:45:53 +0700

    Fix .No index and adding management method.

--- src/import_export/DictionaryCommandline.java
+++ src/import_export/DictionaryCommandline.java
@@ -7,7 +7,7 @@ public class DictionaryCommandline {
         System.out.println("No    | English            | Vietnamese");
 
         for (int i = 0; i < dict.getLength(); ++i) {
-            String no = String.valueOf(i);
+            String no = String.valueOf(i + 1);
             for (int j = 0; j < 6; ++j) {
                 if (j >= no.length()) {
                     System.out.print(' ');
@@ -30,5 +30,11 @@ public class DictionaryCommandline {
             String explain = dict.getWord(i).getWord_explain();
             System.out.println(explain);
         }
+    }
+
+    public void dictionaryBasic() {
+        DictionaryManagement mn = new DictionaryManagement();
+        mn.insertFromCommandline();
+        this.showAllWords(mn.getDict());
     }
 }
