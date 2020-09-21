53392cda5bb833bfde33591e937b5f1b7c9267f6
Darthveloper21 <hienvungoc01@gmail.com>
2020-09-21 10:01:31 +0700

    Add search method

--- tiengviet2vn_dict_1.0/src/import_export/DictionaryManagement.java
+++ tiengviet2vn_dict_1.0/src/import_export/DictionaryManagement.java
@@ -8,6 +8,10 @@ import words_handler.Word;
 public class DictionaryManagement {
     private Dictionary dict = new Dictionary();
 
+    public Dictionary getDict() {
+        return dict;
+    }
+
     public void insertFromCommandline() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -26,9 +30,7 @@ public class DictionaryManagement {
             File input = new File("data/dictionaries.txt");
             Scanner sc = new Scanner(input);
 
-            int n = sc.nextInt();
-
-            for (int i = 0; i < n; ++i) {
+            while (sc.hasNext()) {
                 String target = sc.next();
                 String explain = sc.next();
                 Word word = new Word(target, explain);
@@ -40,7 +42,22 @@ public class DictionaryManagement {
         }
     }
 
-    public Dictionary getDict() {
-        return dict;
+    public void dictionaryLookup() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("\tSearch: ");
+            String s = sc.nextLine();
+            for (int i = 0; i < dict.getLength(); ++i) {
+                Word tmp = dict.getWord(i);
+                if (tmp.getWord_target().toLowerCase().contains(s.toLowerCase()) == true) {
+                    System.out.println(tmp.getWord_target() + " " + tmp.getWord_explain());
+                }
+            }
+
+            if (s.equals("-1")) {
+                break;
+            }
+        }
     }
 }
