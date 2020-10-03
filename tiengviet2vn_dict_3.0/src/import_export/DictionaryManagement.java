eda402ce9d9cbc1030fb06186f2b655f9d1fd76d
Darthveloper21 <hienvungoc01@gmail.com>
2020-10-03 13:44:56 +0700

    Delete unnecessary methods.

--- tiengviet2vn_dict_3.0/src/import_export/DictionaryManagement.java
+++ tiengviet2vn_dict_3.0/src/import_export/DictionaryManagement.java
@@ -7,23 +7,10 @@ import words_handler.Dictionary;
 import words_handler.Word;
 
 public class DictionaryManagement {
-    private Dictionary dict = new Dictionary();
+    private final Dictionary dict = new Dictionary();
 
     public Dictionary getDict() {
         return dict;
-    }
-
-    public void insertFromCommandline() {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; ++i) {
-            String target = sc.next();
-            String explain = sc.next();
-            Word word = new Word(target, explain);
-            dict.addWord(word);
-        }
-        sc.close();
     }
 
     public void insertFromFile() {
@@ -80,25 +67,6 @@ public class DictionaryManagement {
             file.close();
         } catch (Exception e) {
             System.out.println("No path found!");
-        }
-    }
-
-    public void dictionaryLookup() {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("\tSearch: ");
-            String s = sc.nextLine();
-            for (int i = 0; i < dict.getLength(); ++i) {
-                Word tmp = dict.getWord(i);
-                if (tmp.getWord_target().toLowerCase().contains(s.toLowerCase())) {
-                    System.out.println(tmp.getWord_target() + " " + tmp.getWord_explain());
-                }
-            }
-
-            if (s.equals("-1")) {
-                break;
-            }
         }
     }
 }
